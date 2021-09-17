b8b9c055fc6a2a7d180a2ff815366e94eb2e0545
Gustavo Tomás <guga.tomas10@gmail.com>
2021-09-17 11:10:59 -0300

    test

---
+++ server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	static "github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+const PORT = "8080"
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+
+	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	r.Run(":"+PORT)
+}
