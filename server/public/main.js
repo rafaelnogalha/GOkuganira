a8a4a665c08d1dfe3b8048e6e4c113e6accb4a25
Gustavo Tomás <guga.tomas10@gmail.com>
2021-10-02 16:06:25 -0300

    more styling and status update

--- server/public/main.js
+++ server/public/main.js
@@ -2,6 +2,7 @@ const input = document.querySelector('#textarea')
 const messages = document.querySelector('#messages')
 const username = document.querySelector('#username')
 const send = document.querySelector('#send')
+const statusTyping = document.querySelector('#status')
 
 const protocol = window.location.protocol == 'https:' ? 'wss:' : 'ws:';
 const url = `${protocol}//${window.location.host}/ws`;
@@ -18,9 +19,30 @@ const sendMessage = () => {
 	input.value = "";
 };
 
+/**
+ * Update "<username> is typing..." messages
+ */
+
+// TODO: finish typing status
+const updateTyping = msg => {
+	console.log("TEXTAREA CHANGED!")
+	console.log('updateTyping got', msg);
+	// Create a div object which will hold the message
+	const message = document.createElement('div')
+
+	// Set the attribute of the message div
+	message.setAttribute('class', 'status-message')
+	console.log("name: " + msg + " is typing")
+	message.textContent = `${msg} is typing...`
+
+	// Append the message to our chat div
+	statusTyping.appendChild(message)
+};
+
 // The user can send a message either by clicking "Send"
 // or pressing "Enter" (unless shift is pressed).
 send.addEventListener('click', sendMessage);
+input.addEventListener('input', updateTyping);
 input.addEventListener('keyup', evt => {
 	if (evt.key === 'Enter' && !evt.shiftKey) {
 		evt.stopPropagation();
@@ -37,24 +59,16 @@ const insertMessage = messageObj => {
 	const message = document.createElement('div')
 
 	// Set the attribute of the message div
-	message.setAttribute('class', 'chat-message')
 	console.log("name: " + messageObj.username + " content: " + messageObj.content)
 	message.textContent = `${messageObj.username}: ${messageObj.content}`
+	message.setAttribute('class', 'chat-message')
 
 	// Append the message to our chat div
 	messages.appendChild(message)
 
 	// Scroll automatically
-	const elem = document.getElementById("messages")
-	elem.scrollTop = elem.scrollHeight
+	messages.scrollTop = messages.scrollHeight
 }
-
-/**
- * Update "<username> is typing..." messages
- */
-const updateTyping = msg => {
-	console.log('updateTyping got', msg);
-};
 
 const handlers = {
 	'message': insertMessage,
