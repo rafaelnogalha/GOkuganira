6c9c4ada939d3198efe6ebdbab4d8de2a4f22c60
LeoRiether <leonardoalves8217@gmail.com>
2021-10-04 21:32:20 -0300

    js: implemented typing status websocket events
    Most of the work is done in the `typing` variable.
    `typing.started()` and `typing.stopped()` send events through the
    websocket connection to alert everyone else of the current typing
    status.
    
    Still not implemented: correct "<usermame> is typing..." messages. For
    now, every {kind: updateTypingStatus} message is treated as a "typing
    started" event, even though some of them are "typing stopped"

--- server/public/main.js
+++ server/public/main.js
@@ -10,46 +10,95 @@ const ws = new WebSocket(url);
 
 const md = window.markdownit();
 
-const sendMessage = () => {
-	const message = {
-		kind: 'message',
-		username: username.value,
-		content: input.value,
-	}
+const wsSend = data => {
+    ws.send(JSON.stringify({
+        ...data,
+        username: username.value,
+    }));
+};
 
-	ws.send(JSON.stringify(message));
-	input.value = "";
+const sendMessage = () => {
+    wsSend({
+        kind: 'message',
+        content: input.value,
+    });
+
+    input.value = "";
 };
 
 /**
  * Update "<username> is typing..." messages
  */
-
 // TODO: finish typing status
-const updateTyping = msg => {
-	console.log("TEXTAREA CHANGED!")
-	console.log('updateTyping got', msg);
-	// Create a div object which will hold the message
-	const message = document.createElement('div')
+const updateTypingStatus = msg => {
+    console.log("TEXTAREA CHANGED!")
+    console.log('updateTyping got', msg);
+    // Create a div object which will hold the message
+    const message = document.createElement('div')
 
-	// Set the attribute of the message div
-	message.setAttribute('class', 'status-message')
-	console.log("name: " + msg + " is typing")
-	message.textContent = `${msg} is typing...`
+    // Set the attribute of the message div
+    message.setAttribute('class', 'status-message')
+    console.log("name: " + msg.username + " is typing")
+    message.textContent = `${msg} is typing...`
 
-	// Append the message to status div if size < 3
-	if (statusTyping.childElementCount < 3) {
-		statusTyping.appendChild(message)
-	} else {
-		console.log("Already 3 elements!")
-	}
+    // Append the message to status div if size < 3
+    if (statusTyping.childElementCount < 3) {
+        statusTyping.appendChild(message)
+    } else {
+        console.log("Already 3 elements!")
+    }
+};
+
+let typing = {
+    isTyping: false,
+    timer: null,
+    timeout: 3000,
+
+    /**
+     * Sends a {kind: "updateTyping"} message to the websocket
+     * when the user has stopped typing.
+     *
+     * This function is debounced, so it only triggers after the user
+     * has already stopped typing for a while, and not between every
+     * keystroke
+     */
+    stopped() {
+        this.isTyping = false;
+        wsSend({
+            kind: 'updateTyping',
+            isTyping: false,
+        });
+    },
+
+    /**
+     * Sends a {kind: "updateTyping"} message to the websocket
+     * when the user has started typing.
+     *
+     * Resets the timer for the stoppedTyping debounce every time it's
+     * called, but only sends a websocket message if the user has
+     * just started typing
+     */
+    started() {
+        // If we don't get called again in 3 seconds (this.timeout ms),
+        // we should call this.stopped();
+        clearTimeout(this.timer);
+        this.timer = setTimeout(() => { this.stopped(); }, this.timeout);
+
+        if (!this.isTyping) {
+            this.isTyping = true;
+            wsSend({
+                kind: 'updateTyping',
+                isTyping: true,
+            });
+        }
+    },
 };
 
 // The user can send a message either by clicking "Send"
 // or pressing "Enter" (unless shift is pressed).
 send.addEventListener('click', sendMessage);
-input.addEventListener('input', updateTyping);
 input.addEventListener('keyup', evt => {
+    typing.started();
     if (evt.key === 'Enter' && !evt.shiftKey) {
         evt.stopPropagation();
         sendMessage();
@@ -61,32 +110,32 @@ input.addEventListener('keyup', evt => {
  * @param {Message that will be displayed in the UI} messageObj
  */
 const insertMessage = messageObj => {
-  // Create a div object which will hold the message
-  const message = document.createElement('div')
+    // Create a div object which will hold the message
+    const message = document.createElement('div')
 
-  // Set the attribute of the message div
-  message.setAttribute('class', 'chat-message')
-  console.log("name: " + messageObj.username + " content: " + messageObj.content)
-  message.innerHTML = md.renderInline(`${messageObj.username}: ${messageObj.content}`)
+    // Set the attribute of the message div
+    message.setAttribute('class', 'chat-message')
+    console.log("name: " + messageObj.username + " content: " + messageObj.content)
+    message.innerHTML = md.renderInline(`${messageObj.username}: ${messageObj.content}`)
 
-  // Append the message to our chat div
-  messages.appendChild(message)
+    // Append the message to our chat div
+    messages.appendChild(message)
 
-	// Scroll automatically
-	messages.scrollTop = messages.scrollHeight
+    // Scroll automatically
+    messages.scrollTop = messages.scrollHeight
 }
 
 const handlers = {
     'message': insertMessage,
-    'typing': updateTyping,
+    'typing': updateTypingStatus,
 };
 
 ws.onmessage = ({ data }) => {
-	const msg = JSON.parse(data);
-	console.log('got', msg);
-	
-	// Returns if username or msg content is null
-	if (msg.username == "" || msg.content == "") return;
-	const handler = handlers[msg.kind];
-	handler(msg);
+    const msg = JSON.parse(data);
+    console.log('got', msg);
+
+    // Returns if username or msg content is null
+    if (msg.username == "" || msg.content == "") return;
+    const handler = handlers[msg.kind];
+    handler(msg);
 };
