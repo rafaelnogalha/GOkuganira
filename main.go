d1664572145f2b72ed25dcfefa266295a884fefd
Gustavo Tomás <guga.tomas10@gmail.com>
2021-10-18 15:24:31 -0300

    removed database & cleanup :)

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"GOkuganira/controllers"
-	"GOkuganira/utils"
 	"fmt"
 	"net/http"
 	"os"
@@ -16,14 +14,8 @@ func main() {
 	port := os.Getenv("PORT");
 	r := gin.Default()
 	m := melody.New()
-	db := utils.SetupModels() //new database
 
 	r.Use(static.Serve("/", static.LocalFile("./server/public", true)))
-
-	r.Use(func(c *gin.Context) {
-		c.Set("db", db)
-		c.Next()
-	})
 
 	// Get main chat
 	r.GET("/ws", func(c *gin.Context) {
@@ -39,12 +31,6 @@ func main() {
 		m.HandleRequest(c.Writer, c.Request)
 	})
 
-	// Get all users at the database
-	r.GET("/users", controllers.FindUsers)
-
-	// Post one user at database
-	r.POST("/users", controllers.CreateUser)
-
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
 		m.BroadcastFilter(msg, func(q *melody.Session) bool {
 			cond := q.Request.URL.Path == s.Request.URL.Path
@@ -52,17 +38,8 @@ func main() {
 		})
 	})
 
-	// TODO: see if can be done with username instead of IP :)
 	m.HandleConnect(func(s *melody.Session) {
-		// fmt.Println("Number of active sessions: ", m.Len())
-		// ip := GetLocalIP()
-		// msg := `{
-		// 	"kind": "message",
-		// 	"username":"` + ip + `",
-		// 	"content": "Resolveu estragar a conversa!"
-		// }`
-
-		// m.BroadcastOthers([]byte(msg), s)
+		fmt.Println("Session connected: ", m.Len())
 	})
 	
 	m.HandleDisconnect(func(s *melody.Session){
--- models/exception.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type Exception struct {
-	Message string `json:"message"`
-}
